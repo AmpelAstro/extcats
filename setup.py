8738ddb9f366af8705ecd43b460946b6a875d5cf
MatteoGiomi <matteo.giomi@desy.de>
2018-02-22 12:34:24 +0100

    added url and download fields

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ from shutil import unpack_archive
 from setuptools import setup
 
 setup(
-   name='extcats',
-   version='1.1',
-   description='Tools to organize and query astronomical catalogs',
-   author='Matteo Giomi',
-   author_email='matteo.giomi@desy.de',
-   packages=['extcats'],
-   install_requires=['pymongo', 'healpy', 'astropy', 'pandas', 'tqdm'],
-)
+    name='extcats',
+    version='1.1',
+    description='Tools to organize and query astronomical catalogs',
+    author='Matteo Giomi',
+    author_email='matteo.giomi@desy.de',
+    packages=['extcats'],
+    url = 'https://github.com/MatteoGiomi/extcats',
+    download_url = 'https://github.com/MatteoGiomi/extcats/archive/1.1.tar.gz',
+    install_requires=['pymongo', 'healpy', 'astropy', 'pandas', 'tqdm'],
+    )
 
 
 # fuffa
