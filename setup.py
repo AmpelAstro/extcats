96fdf6fc3192ba08d5c703c7d5b26d763d14726f
MatteoGiomi <matteo.giomi@desy.de>
2018-02-22 13:42:36 +0100

    upped version number

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ from setuptools import setup
 
 setup(
     name='extcats',
-    version='1.1',
+    version='1.2',
     description='Tools to organize and query astronomical catalogs',
     author='Matteo Giomi',
     author_email='matteo.giomi@desy.de',
     packages=['extcats'],
     url = 'https://github.com/MatteoGiomi/extcats',
-    download_url = 'https://github.com/MatteoGiomi/extcats/archive/1.1.tar.gz',
+    download_url = 'https://github.com/MatteoGiomi/extcats/archive/1.2.tar.gz',
     install_requires=['pymongo', 'healpy', 'astropy', 'pandas', 'tqdm'],
     )
 
