787e9420a76d1f4b2e29192c74e4966488c7a7ab
sjoertvv <sjoert@nyu.edu>
2018-04-03 17:25:42 -0400

    simple exception statement for python2 users

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@
 # Author: M. Giomi (matteo.giomi@desy.de)
 
 import os
-from urllib.request import urlretrieve
-from shutil import unpack_archive
 from setuptools import setup
 
 setup(
@@ -29,18 +27,26 @@ if not os.path.isdir(mqc_testdata_dir):
     print ("creating testdata directory: %s"%mqc_testdata_dir)
     os.makedirs(mqc_testdata_dir)
 dest = os.path.join(mqc_testdata_dir, "heasarc_milliquas.tdat.gz")
-if not os.path.isfile(dest):
-    print ("downloading the million quasar catalog \
-(MQC, https://heasarc.gsfc.nasa.gov/W3Browse/all/milliquas.html)")
-    urlretrieve("https://desycloud.desy.de/index.php/s/AsxkEJUJ8LrgJkd/download", dest)
+try:
+    from urllib.request import urlretrieve
+    from shutil import unpack_archive
 
-# a few small files from Panstarrs
-ps1_testdata_dir = "./testdata/PS1DR1_test"
-if not os.path.isdir(ps1_testdata_dir):
-    print ("creating testdata directory: %s"%ps1_testdata_dir)
-    os.makedirs(ps1_testdata_dir)
-if not os.listdir(ps1_testdata_dir):
-    print ("downloading few PS1 DR1 MeanObject table files (https://panstarrs.stsci.edu/)")
-    tmp = os.path.join(ps1_testdata_dir, "tmp_PS1DR1_test.zip")
-    urlretrieve("https://desycloud.desy.de/index.php/s/3AAJ30J7YZ7dH43/download", tmp)
-    unpack_archive(tmp, "./testdata")
+    if not os.path.isfile(dest):
+
+        print ("downloading the million quasar catalog \
+    (MQC, https://heasarc.gsfc.nasa.gov/W3Browse/all/milliquas.html)")
+        urlretrieve("https://desycloud.desy.de/index.php/s/AsxkEJUJ8LrgJkd/download", dest)
+
+    # a few small files from Panstarrs
+    ps1_testdata_dir = "./testdata/PS1DR1_test"
+    if not os.path.isdir(ps1_testdata_dir):
+        print ("creating testdata directory: %s"%ps1_testdata_dir)
+        os.makedirs(ps1_testdata_dir)
+    if not os.listdir(ps1_testdata_dir):
+        print ("downloading few PS1 DR1 MeanObject table files (https://panstarrs.stsci.edu/)")
+        tmp = os.path.join(ps1_testdata_dir, "tmp_PS1DR1_test.zip")
+        urlretrieve("https://desycloud.desy.de/index.php/s/3AAJ30J7YZ7dH43/download", tmp)
+        unpack_archive(tmp, "./testdata")
+except ImportError:
+    print ('\nImportError on urllib.request or shutil (likely python2 related)')
+
