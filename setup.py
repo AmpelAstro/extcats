949391a71fdaba655a0709877e8a213beba6d707
Jakob van Santen <jvansanten@gmail.com>
2021-02-10 13:18:13 +0100

    Run tests in GA

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@
 from setuptools import setup
 
 setup(
-    name='extcats',
-    version='2.4',
-    description='Tools to organize and query astronomical catalogs',
-    author='Matteo Giomi',
-    author_email='matteo.giomi@desy.de',
-    packages=['extcats'],
-    url = 'https://github.com/MatteoGiomi/extcats',
-    download_url = 'https://github.com/MatteoGiomi/extcats/archive/2.4.tar.gz',
-    install_requires=['pymongo', 'healpy', 'astropy', 'pandas', 'tqdm'],
-    )
+    name="extcats",
+    version="2.4",
+    description="Tools to organize and query astronomical catalogs",
+    author="Matteo Giomi",
+    author_email="matteo.giomi@desy.de",
+    packages=["extcats"],
+    url="https://github.com/AmpelProject/extcats",
+    download_url="https://github.com/AmpelProject/extcats/archive/2.4.tar.gz",
+    install_requires=["pymongo", "healpy", "astropy", "pandas", "tqdm"],
+    extras_require={"testing": ["pytest"]},
+)
